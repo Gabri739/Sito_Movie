ad2510f6eda400c91b3b01034b3f4ac94c83007c
Gabri739 <gabriele.bragaglia@outlook.it>
2025-05-15 09:04:03 +0200

    Aggiunte classi Entity User e Entity Credentials
    con i relativi metodi getter/setter equals e hashcode

---
+++ siw-movie-01/src/main/java/it/uniroma3/siw/model/Credentials.java
@@ -0,0 +1,76 @@
+package it.uniroma3.siw.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Credentials {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String username;
+	
+	private String role;
+	
+	@OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL )
+	private User user;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, role, user, username);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Credentials other = (Credentials) obj;
+		return Objects.equals(id, other.id) && Objects.equals(role, other.role) && Objects.equals(user, other.user)
+				&& Objects.equals(username, other.username);
+	}
+	
+}
